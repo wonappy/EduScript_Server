c3cae7f2630bdb36b89a3ffa54b02bac856fabf2
suhyeon <102345466+jeong-su-hyeon@users.noreply.github.com>
2025-06-18 19:17:41 +0900

    Update 발화 정제 엔드포인트 통합
    bool형 변수를 통해 요약 or 내용 추출 처리하도록 수정함

--- src/app/services/language_service.py
+++ src/app/services/language_service.py
@@ -1,9 +1,7 @@
 # [serivces/language_service.py]
 # LLM 작업 로직 처리 
-from fastapi import HTTPException
-import logging
 from src.app.modules.llm.openai_llm import OpenAILLM
-from src.app.dto.refinement_dto import SpeechRefineRequest, SpeechSummarizeRequest, KeyPointsExtractRequest, SpeechRefineResponse, SpeechSummarizeResponse, KeyPointsExtractResponse
+from src.app.dto.refinement_dto import SpeechRefineRequest, SpeechRefineResponse
 from src.app.prompts.refining_prompt import refine_speech_prompt
 from src.app.prompts.summarizing_prompt import summarize_speech_prompt
 from src.app.prompts.keypoints_prompt import extract_keypoints_prompt
@@ -12,67 +10,80 @@ from src.app.prompts.keypoints_prompt import extract_keypoints_prompt
 llm_module = OpenAILLM() 
 
 # [1] 발화 정제
-async def refine_speech_service(request: SpeechRefineRequest) -> SpeechRefineResponse:
-    try:
-        # 🔴 디버깅
-        print(f"=== [SERVICE DEBUG 1] refine_speech_service ===") 
-        print(f"[SERVICE] full_speech: {request.full_speech if hasattr(request, 'full_speech') else 'None'}")
-        
-        # 1) 프롬프트 설계
+async def refine_text_service(request: SpeechRefineRequest) -> SpeechRefineResponse:
+    try:        
+        # [0] 디버깅 코드
+        print("=== [SERVICE DEBUG] ===")
+        print(">> Refine")
+        print(f"[SERVICE] full_text: {request.full_text}")
+        print(f"[SERIVCE] enable_refine: {request.enable_refine}")
+        print(f"[SERIVCE] enable_summarize: {request.enable_summarize}")
+        print(f"[SERIVCE] enable_keypoints: {request.enable_keypoints}")
+                
+        # 1) 정제 프롬프트 설계
         messages = [
             { "role": "system", "content": refine_speech_prompt() },
-            { "role": "user", "content": f" 다음 발화를 정제해주세요 : {request.full_speech}" } 
-         ]        
-        # 2) LLM 모듈 호출 
-        refined_text = await llm_module.select_gpt_model(messages=messages)        
-        # 3) 응답 DTO 
-        return SpeechRefineResponse(refined_speech=refined_text)
-    except HTTPException as e:
-        logging.error(f"[SERVICE ERROR] 발화 정제 HTTP 에러 발생 - [{e.status_code}: {e.detail}]")
-        raise
+            { "role": "user", "content": f"{request.full_text}" } 
+         ] 
+        
+        # 저장 변수 선언 
+        refined_text = None
+        summarized_text = None
+        keypoints_text = None  
+
+        # # 2) LLM 모듈 호출 (API) -> 정제 처리     
+        temp_refined_text = await llm_module.select_gpt_model(messages=messages)  
+        # 정제 반환 여부 
+        if request.enable_refine == True:
+            refined_text = temp_refined_text
+        # 3) 요약 처리    
+        if request.enable_summarize == True:
+            summarized_text = await summarize_text(temp_refined_text)
+        # 4) 중요 내용 추출 처리
+        if request.enable_keypoints == True:
+            keypoints_text = await extract_keypoints(temp_refined_text) 
+
+        # 5) 응답 결과 DTO (정제, 요약, 추출)
+        return SpeechRefineResponse(refined_result=refined_text, summarized_result=summarized_text, keypoints_result=keypoints_text)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"[SERVICE ERROR] 발화 정제 실패 - {str(e)}")
+        raise Exception(f"[SERVICE ERROR] 발화 정제 실패 - {str(e)}")
 
 # [2] 발화 요약
-async def summarize_speech_service(request: SpeechSummarizeRequest) -> SpeechSummarizeResponse:
-    try:
-        # 🔴 디버깅
-        print(f"=== [SERVICE DEBUG 2] summarize_speech_service ===") 
-        print(f"[SERVICE] refined_speech: {request.refined_speech if hasattr(request, 'refined_speech') else 'None'}")
+async def summarize_text(temp_refined_text: str) -> str:
+    try:        
+        # [0] 디버깅 코드
+        print(">> Summarize")
+        print(f"[SERVICE] refined_text: {temp_refined_text}")
         
-        # 1) 프롬프트 설계
+        # 1) 내용 요약 프롬프트 설계
         messages = [
             { "role": "system", "content": summarize_speech_prompt() },
-            { "role": "user", "content": f"다음 발화를 요약해주세요 : {request.refined_speech}" }
+            { "role": "user", "content": f"{temp_refined_text}" }
         ]
-        # 2) LLM 모듈 호출 
+        # 2) LLM API 호출 (MODULE) -> 내용 요약 처리
         summarized_text = await llm_module.select_gpt_model(messages=messages)
-        # 3) 응답 DTO
-        return SpeechSummarizeResponse(summarized_speech=summarized_text)
-    except HTTPException as e:
-        logging.error(f"[SERVICE ERROR] 발화 요약 HTTP 에러 발생 - [{e.status_code}: {e.detail}]")
-        raise
+        # 3) 요약 결과 문자열 
+        return summarized_text
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"[SERIVCE ERROR] 발화 요약 처리 실패 - {str(e)}")
+        raise Exception(f"[SERIVCE ERROR] 발화 요약 처리 실패 - {str(e)}")
 
 # [3] 중요 내용 추출
-async def extract_keypoints_service(request: KeyPointsExtractRequest) -> KeyPointsExtractResponse:
-    try:
-        # 🔴 디버깅
-        print(f"=== [SERVICE DEBUG 3] extract_keypoints_service ===") 
-        print(f"[SERVICE] refined_speech: {request.refined_speech if hasattr(request, 'refined_speech') else 'None'}")
+async def extract_keypoints(temp_refined_text: str) -> str:
+    try:        
+        # [0] 디버깅 코드
+        print(">> Extract Keypoints") 
+        print(f"[SERVICE] refined_text: {temp_refined_text}")
 
-        # 1) 프롬프트 설계
+        # 1) 중요 내용 추출 프롬프트 설계
         messages = [
             { "role": "system", "content": extract_keypoints_prompt() },
-            { "role": "user", "content": f"다음 발화를 요약해주세요 : {request.refined_speech}" }
+            { "role": "user", "content": f"{temp_refined_text}" }
         ]
-        # 2) LLM 모듈 호출 
-        extracted_text = await llm_module.select_gpt_model(messages=messages)
-        # 3) 응답 DTO
-        return KeyPointsExtractResponse(extracted_keypoints=extracted_text)
-    except HTTPException as e:
-        logging.error(f"[SERVICE ERROR] 발화 요약 HTTP 에러 발생 - [{e.status_code}: {e.detail}]")
-        raise
+
+        # 2) LLM API 호출 (MODULE) -> 중요 내용 추출 처리
+        keypoints_text = await llm_module.select_gpt_model(messages=messages)
+
+        # 3) 추출 결과 반환 
+        return keypoints_text
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"[SERIVCE ERROR] 발화 요약 처리 실패 - {str(e)}")
+        raise Exception(f"[SERIVCE ERROR] 중요 내용 추출 실패 - {str(e)}")
